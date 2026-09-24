6fda6bde42154ebc56b8bf29e2df1c58ad8f8ae4
Abhay Verma <162298236+Abhay15-11@users.noreply.github.com>
2026-09-25 00:34:21 +0530

    Change app.run to use host and port from environment

--- app.py
+++ app.py
@@ -57,4 +57,4 @@ def add_product():
     return render_template('add_product.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0',port = int(os.environ.get('PORT',5000)))
